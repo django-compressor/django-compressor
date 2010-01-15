9e148cdd90c61659c2f6e2d8b9172a4342fb63c0
Aaron Godfrey <agodfrey@AGod-desktop.(none)>
2010-01-15 11:00:38 -0500

    Made the CSSTidyFilter function.
    Signed-off-by: Jannis Leidel <jannis@leidel.info>

--- compressor/filters/csstidy.py
+++ compressor/filters/csstidy.py
@@ -1,6 +1,6 @@
-import os
-import warnings
+from subprocess import Popen, PIPE
 import tempfile
+import warnings
 
 from django.conf import settings
 
@@ -18,16 +18,17 @@ class CSSTidyFilter(FilterBase):
         tmp_file.flush()
 
         output_file = tempfile.NamedTemporaryFile(mode='w+b')
-        
+
         command = '%s %s %s %s' % (BINARY, tmp_file.name, ARGUMENTS, output_file.name)
-        
-        command_output = os.popen(command).read()
-        
+
+        command_output = Popen(command, shell=True,
+            stdout=PIPE, stdin=PIPE, stderr=PIPE).communicate()
+
         filtered_css = output_file.read()
         output_file.close()
         tmp_file.close()
-        
+
         if self.verbose:
             print command_output
-        
+
         return filtered_css
