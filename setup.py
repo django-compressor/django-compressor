b1040b07e4bb76641ba4f16283c70b6f6fa3e9bc
Jannis Leidel <jannis@leidel.info>
2010-02-23 12:10:10 +0100

    Bumped version to 0.5.2.

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ README = read('README.rst')
 
 setup(
     name = "django_compressor",
-    version = "0.5.1",
+    version = "0.5.2",
     url = 'http://github.com/mintchaos/django_compressor',
     license = 'BSD',
     description = "Compresses linked and inline javascript or CSS into a single cached file.",
-    long_description=README,
+    long_description = README,
 
     author = 'Christian Metts',
     author_email = 'xian@mintchaos.com',
