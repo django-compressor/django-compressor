53e843eec4a8cca59ccaa4046138810efcec2784
Ulrich Petri <ulo@ulo.pe>
2011-01-12 00:10:06 +0100

    Added mgmt command options + verbose output

--- compressor/management/commands/compress.py
+++ compressor/management/commands/compress.py
@@ -1,7 +1,10 @@
+import os
+import sys
+import warnings
+
 from django.core.management.base import  NoArgsCommand, CommandError
 from django.template.loader import find_template_loader
 from compressor.conf import settings
-import os
 from fnmatch import fnmatch
 from django.template import Template, TemplateSyntaxError
 from django.conf import settings as django_settings
@@ -9,6 +12,8 @@ from compressor.templatetags.compress import CompressorNode
 from django.template.context import Context
 from django.utils.simplejson.decoder import JSONDecoder
 from optparse import make_option
+from django.core.cache import cache
+from compressor.utils import make_offline_cache_key
 
 __author__ = 'ulo'
 
@@ -17,9 +22,21 @@ class Command(NoArgsCommand):
         make_option('-c', '--context', default="", dest='context',
             help="""Context to use while rendering the 'compress' nodes."""
                 """ (In JSON format; e.g.: '{"something": 1, "other": "value"}'"""),
+        make_option('-f', '--force', default=False, action="store_true", dest='force',
+            help="""Force generation of offline cache even if settings.COMPRESS_OFFLINE is not set."""),
     )
 
     def handle_noargs(self, **options):
+        verbosity = int(options.get("verbosity", 0))
+
+        if not settings.COMPRESS:
+            raise CommandError("Compressor is disabled. Set COMPRESS settting to True (or DEBUG to False) to enable.")
+        
+        if not settings.COMPRESS_OFFLINE:
+            if not options.get("force"):
+                raise CommandError("Aborting; COMPRESS_OFFLINE is not set. (Use -f to override)")
+            warnings.warn("COMPRESS_OFFLINE is not set. Offline generated cache will not be used.")
+
         paths = []
         for loader in settings.TEMPLATE_LOADERS:
             loader_class = find_template_loader(loader)
@@ -31,6 +48,11 @@ class Command(NoArgsCommand):
         if not paths:
             raise CommandError("No template paths found. You need to configure settings.TEMPLATE_LOADERS.")
 
+        if verbosity > 1:
+            sys.stdout.write("Considering paths:\n\t")
+            sys.stdout.write("\n\t".join(paths))
+            print
+            
         template_files = []
         for path in paths:
             for root, dirs, files in os.walk(path):
@@ -39,7 +61,12 @@ class Command(NoArgsCommand):
         if not template_files:
             raise CommandError("No templates found. You need to configure settings.TEMPLATE_LOADERS.")
 
-        compressor_nodes = []
+        if verbosity > 1:
+            sys.stdout.write("Found templates:\n\t")
+            sys.stdout.write("\n\t".join(template_files))
+            print
+
+        compressor_nodes = {}
         for template_filename in template_files:
             try:
                 template_file = open(template_filename)
@@ -48,24 +75,40 @@ class Command(NoArgsCommand):
                 finally:
                     template_file.close()
             except IOError: # unreadable file -> ignore
+                if verbosity > 0:
+                    print "Unreadable template at: %s" % template_filename
                 continue
             except TemplateSyntaxError: # broken template -> ignore
+                if verbosity > 0:
+                    print "Invalid template at: %s" % template_filename
                 continue
 
-            compressor_nodes.extend(self.walk_nodes(template))
+            nodes = self.walk_nodes(template)
+            if nodes:
+                compressor_nodes.setdefault(template_filename, []).extend(nodes)
 
         if not compressor_nodes:
             raise CommandError("No 'compress' template tags found in templates.")
 
-        optional_context = {}
+        if verbosity > 0:
+            sys.stdout.write("Found 'compress' tags in:\n\t")
+            sys.stdout.write("\n\t".join(compressor_nodes.keys()))
+            print
+
+        context_content = {'_ignore_offline_setting': True}
         if "context" in options and options['context']:
             try:
-                optional_context = JSONDecoder().decode(options['context'])
+                context_content.update(JSONDecoder().decode(options['context']))
             except ValueError, e:
                 raise CommandError("Invalid context JSON specified.", e)
-        for node in compressor_nodes:
-            result = node.render(Context(optional_context))
-            print result
+
+        count = 0
+        for filename, nodes in compressor_nodes.items():
+            for node in nodes:
+                key = make_offline_cache_key(node.nodelist)
+                result = node.render(Context(context_content))
+                cache.set(key, result, settings.OFFLINE_TIMEOUT)
+                count += 1
 
     def walk_nodes(self, start_node):
         compressor_nodes = []
