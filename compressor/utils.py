0692c3e4ed623624db893538326fe640f1541d49
Jannis Leidel <jannis@leidel.info>
2011-02-01 01:04:36 +0100

    Backport importlib from Django (thanks!).

--- compressor/utils.py
+++ compressor/utils.py
@@ -1,8 +1,9 @@
 import os
+import sys
+
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.encoding import smart_str
 from django.utils.hashcompat import sha_constructor
-from django.utils.importlib import import_module
 
 from compressor.cache import cache
 from compressor.conf import settings
@@ -88,3 +89,37 @@ def walk(root, topdown=True, onerror=None, followlinks=False):
                         yield (link_dirpath, link_dirnames, link_filenames)
 
 
+# Taken from Django 1.3-beta1 and before that from Python 2.7 with permission from/by the original author.
+def _resolve_name(name, package, level):
+   """Return the absolute name of the module to be imported."""
+   if not hasattr(package, 'rindex'):
+       raise ValueError("'package' not set to a string")
+   dot = len(package)
+   for x in xrange(level, 1, -1):
+       try:
+           dot = package.rindex('.', 0, dot)
+       except ValueError:
+           raise ValueError("attempted relative import beyond top-level "
+                             "package")
+   return "%s.%s" % (package[:dot], name)
+
+
+def import_module(name, package=None):
+   """Import a module.
+
+   The 'package' argument is required when performing a relative import. It
+   specifies the package to use as the anchor point from which to resolve the
+   relative import to an absolute import.
+
+   """
+   if name.startswith('.'):
+       if not package:
+           raise TypeError("relative imports require the 'package' argument")
+       level = 0
+       for character in name:
+           if character != '.':
+               break
+           level += 1
+       name = _resolve_name(name[level:], package, level)
+   __import__(name)
+   return sys.modules[name]
