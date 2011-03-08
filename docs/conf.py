94a386b2be4d01512e2ba14610021865651537d4
Jannis Leidel <jannis@leidel.info>
2011-03-08 22:44:50 +0100

    Stop using an own theme, now that Read The Docs' default theme is nice.

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ pygments_style = 'murphy'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'compressor'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -99,7 +99,7 @@ html_theme = 'compressor'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['_theme']
+# html_theme_path = ['_theme']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
