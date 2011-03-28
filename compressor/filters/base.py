fe80fa9ef0ac3e830809e21dd87fc0671d0415fd
Jannis Leidel <jannis@leidel.info>
2011-03-28 19:17:21 +0200

    Moved base filter to base module.

---
+++ compressor/filters/base.py
@@ -0,0 +1,24 @@
+import logging
+
+import subprocess
+
+from compressor.conf import settings
+from compressor.exceptions import FilterError
+from compressor.utils import cmd_split
+
+logger = logging.getLogger("compressor.filters")
+
+class FilterBase(object):
+
+    def __init__(self, content, filter_type=None, verbose=0):
+        self.type = filter_type
+        self.content = content
+        self.verbose = verbose or settings.COMPRESS_VERBOSE
+        self.logger = logger
+
+    def input(self, **kwargs):
+        raise NotImplementedError
+
+    def output(self, **kwargs):
+        raise NotImplementedError
+
