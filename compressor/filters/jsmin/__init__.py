0667a92e9017217838b78c3a6bb1c8fd973516c5
Jannis Leidel <jannis@leidel.info>
2011-04-27 14:48:01 +0200

    Stop using the jsmin.py implementation and rely on rjmsin instead. Should be another speed improvment, http://opensource.perlig.de/rjsmin/benchmark.html. Fixes issue #39.

--- compressor/filters/jsmin/__init__.py
+++ compressor/filters/jsmin/__init__.py
@@ -1,5 +1,7 @@
-from compressor.filters.jsmin.jsmin import jsmin
 from compressor.filters import FilterBase
+from compressor.filters.jsmin.rjsmin import _make_jsmin
+
+jsmin = _make_jsmin(python_only=True)
 
 class JSMinFilter(FilterBase):
     def output(self, **kwargs):
