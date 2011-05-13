fc951bcae04d669eaede9d87cd45666daf0be27c
Jannis Leidel <jannis@leidel.info>
2011-05-13 13:36:52 +0200

    Whitespace cleanup.

--- compressor/tests/precompiler.py
+++ compressor/tests/precompiler.py
@@ -4,24 +4,24 @@ import sys
 
 def main():
     p = optparse.OptionParser()
-    p.add_option('-f', '--file', action="store", 
-                type="string", dest="filename", 
+    p.add_option('-f', '--file', action="store",
+                type="string", dest="filename",
                 help="File to read from, defaults to stdin", default=None)
     p.add_option('-o', '--output', action="store",
-                type="string", dest="outfile", 
+                type="string", dest="outfile",
                 help="File to write to, defaults to stdout", default=None)
-                
+
     options, arguments = p.parse_args()
-    
+
     if options.filename:
         f = open(options.filename)
         content = f.read()
         f.close()
     else:
         content = sys.stdin.read()
-    
+
     content = content.replace('background:', 'color:')
-    
+
     if options.outfile:
         f = open(options.outfile, 'w')
         f.write(content)
@@ -29,7 +29,6 @@ def main():
     else:
         print content
 
- 
+
 if __name__ == '__main__':
     main()
-    
