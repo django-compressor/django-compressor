db63f8b026ed773720e074bac21b6f1e9a79931d
David Medina <davidmedina9@gmail.com>
2011-07-14 15:52:47 +0200

    Fix compass config, less restrictive

--- compressor/compass/conf.py
+++ compressor/compass/conf.py
@@ -7,23 +7,25 @@ from compressor.utils.settings import AppSettings
 
 class CompassSettings(AppSettings):
 
-    ENABLED = global_settings.DEBUG
+    ENABLED = False
     CONFIG = 'config.rb'
     WHERE = ''
     BINARY = 'compass'
 
     def configure_config(self, value):
+        enabled = getattr(global_settings, 'COMPASS_ENABLED', None)
         config_path = os.path.join(compressor_settings.COMPRESS_ROOT, value)
-        if not os.path.isfile(config_path):
+        if not os.path.isfile(config_path) and enabled:
             raise ImproperlyConfigured("COMPASS_CONFIG setting is not a config \
                 file to compass: %s" % config_path)
         return os.path.abspath(config_path)
 
     def configure_where(self, value):
+        enabled = getattr(global_settings, 'COMPASS_ENABLED', None)
         if not value:
             value = os.path.abspath(os.path.dirname(self.COMPASS_CONFIG))
         else:
-            if not os.path.isdir(value):
+            if not os.path.isdir(value) and enabled:
                 raise ImproperlyConfigured("COMPASS_WHERE setting is not set to \
                     a valid directory to generate compass files: %s" % value)
         return value
