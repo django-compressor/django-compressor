55da5e07c2a5af5e8388b7fae0eb00696e931d89
Jannis Leidel <jannis@leidel.info>
2011-08-09 10:09:20 +0200

    Prepare addition of Slimit filter.

--- compressor/filters/jsmin/__init__.py
+++ compressor/filters/jsmin/__init__.py
@@ -3,6 +3,9 @@ from compressor.filters.jsmin.rjsmin import _make_jsmin
 
 jsmin = _make_jsmin(python_only=True)
 
-class JSMinFilter(FilterBase):
+class rJSMinFilter(FilterBase):
     def output(self, **kwargs):
         return jsmin(self.content)
+
+# This is for backwards compatibility
+JSMinFilter = rJSMinFilter
