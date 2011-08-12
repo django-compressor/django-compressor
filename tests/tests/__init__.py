de88128fb39ac5d15fe606fff6809ed04dec5490
Jannis Leidel <jannis@leidel.info>
2011-08-12 11:53:43 +0200

    Further removal of CompassFilter.

--- tests/tests/__init__.py
+++ tests/tests/__init__.py
@@ -1,5 +1,5 @@
 from .base import CompressorTestCase, CssMediaTestCase, VerboseTestCase, CacheBackendTestCase
-from .filters import CssTidyTestCase, CompassTestCase, PrecompilerTestCase, CssMinTestCase, CssAbsolutizingTestCase, CssDataUriTestCase
+from .filters import CssTidyTestCase, PrecompilerTestCase, CssMinTestCase, CssAbsolutizingTestCase, CssDataUriTestCase
 from .offline import OfflineGenerationTestCase
 from .parsers import LxmlParserTests, Html5LibParserTests, BeautifulSoupParserTests, HtmlParserTests
 from .storages import StorageTestCase
