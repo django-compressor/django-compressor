22f6245d1c60f65c105816e96cdf4c08ad188aa8
Jannis Leidel <jannis@leidel.info>
2011-09-23 15:32:00 +0200

    Made sure the rel attribute of link tags can be mixed case. Fixes #125.

--- tests/tests/templatetags.py
+++ tests/tests/templatetags.py
@@ -45,6 +45,15 @@ class TemplatetagTestCase(TestCase):
         out = css_tag("/media/CACHE/css/e41ba2cc6982.css")
         self.assertEqual(out, render(template, self.context))
 
+    def test_uppercase_rel(self):
+        template = u"""{% load compress %}{% compress css %}
+        <link rel="StyleSheet" href="{{ MEDIA_URL }}css/one.css" type="text/css">
+        <style type="text/css">p { border:5px solid green;}</style>
+        <link rel="StyleSheet" href="{{ MEDIA_URL }}css/two.css" type="text/css">
+        {% endcompress %}"""
+        out = css_tag("/media/CACHE/css/e41ba2cc6982.css")
+        self.assertEqual(out, render(template, self.context))
+
     def test_nonascii_css_tag(self):
         template = u"""{% load compress %}{% compress css %}
         <link rel="stylesheet" href="{{ MEDIA_URL }}css/nonasc.css" type="text/css">
