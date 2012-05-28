98cb3ef143756ac176457edb5b12c78973cafe82
Vladislav Poluhin <nuklea@gmail.com>
2012-05-28 14:07:21 +0800

    Fixes UnicodeDecodeError with inline css or javascript (closes #270)

--- compressor/tests/test_jinja2ext.py
+++ compressor/tests/test_jinja2ext.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import with_statement
 
 from django.test import TestCase
@@ -122,4 +123,16 @@ class TestJinja2CompressorExtension(TestCase):
         {% endcompress %}""")
         context = {'MEDIA_URL': settings.COMPRESS_URL}
         out = '<script type="text/javascript">obj={};obj.value="value";</script>'
+        self.assertEqual(out, template.render(context))
+
+    def test_nonascii_inline_css(self):
+        org_COMPRESS_ENABLED = settings.COMPRESS_ENABLED
+        settings.COMPRESS_ENABLED = False
+        template = self.env.from_string(u'{% compress css %}'
+                                        u'<style type="text/css">'
+                                        u'/* русский текст */'
+                                        u'</style>{% endcompress %}')
+        out = u'<link rel="stylesheet" href="/media/CACHE/css/b2cec0f8cb24.css" type="text/css" />'
+        settings.COMPRESS_ENABLED = org_COMPRESS_ENABLED
+        context = {'MEDIA_URL': settings.COMPRESS_URL}
         self.assertEqual(out, template.render(context))
