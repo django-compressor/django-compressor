a2cd5d50ee858ad98552b0d89cff6997d2a675ee
Saul Shanabrook <s.shanabrook@gmail.com>
2012-11-04 09:28:36 -0500

    Pep8 Fixes

--- compressor/contrib/sekizai.py
+++ compressor/contrib/sekizai.py
@@ -8,11 +8,11 @@
 from compressor.templatetags.compress import CompressorNode
 from django.template.base import Template
 
+
 def compress(context, data, name):
     """
     Data is the string from the template (the list of js files in this case)
     Name is either 'js' or 'css' (the sekizai namespace)
-
-    We basically just manually pass the string through the {% compress 'js' %} template tag
+    Basically passes the string through the {% compress 'js' %} template tag
     """
     return CompressorNode(nodelist=Template(data).nodelist, kind=name, mode='file').render(context=context)
