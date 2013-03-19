de99e20c4738da9bba060e0aff5d2f5f715e8829
sodas <sodas2002@gmail.com>
2013-03-19 08:02:40 +0800

    django.utils.simplejson is deprecated
    django.utils.simplejson has been deprecated from django 1.5.
    (django 1.5 supports only Python 2.6+ which shipped with a built-in json lib)

--- compressor/cache.py
+++ compressor/cache.py
@@ -5,7 +5,10 @@ import time
 
 from django.core.cache import get_cache
 from django.core.files.base import ContentFile
-from django.utils import simplejson
+try:
+    import json as simplejson
+except ImportError:
+    from django.utils import simplejson
 from django.utils.encoding import smart_str
 from django.utils.functional import SimpleLazyObject
 from django.utils.importlib import import_module
