a20c9051d4efaf4d2acf5693d995a7c27c7b7ac6
Matthew Scott <matt@11craft.com>
2013-04-03 20:34:39 -0500

    Encode compiled content when writing it w/o compression
    As of #312, the output of compiled content is always unicode.
    
    With ``COMPRESS_ENABLED=True``,
    that unicode was piped directly to compression
    without writing the intermediate content.
    The byte encoding of the compressed content
    is handled correctly in this case.
    
    However, with ``COMPRESS_ENABLED=False``,
    the unicode content was sent directly to a
    ``django.core.files.base.ContentFile`` instance,
    and somewhere in there
    it turned that content into 4-byte-wide characters
    resembling UTF-32 encoded content.
    
    This encodes that content using UTF-8
    before handing it off to the ``ContentFile``.
    
    This may present an issue in a corner case
    where something other than UTF-8 encoding is desired.

--- compressor/base.py
+++ compressor/base.py
@@ -271,7 +271,7 @@ class Compressor(object):
         """
         new_filepath = self.get_filepath(content, basename=basename)
         if not self.storage.exists(new_filepath) or forced:
-            self.storage.save(new_filepath, ContentFile(content))
+            self.storage.save(new_filepath, ContentFile(content.encode('utf8')))
         url = mark_safe(self.storage.url(new_filepath))
         return self.render_output(mode, {"url": url})
 
