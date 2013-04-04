0703c961f82b1f86f3ec737e93cb5d9493017548
Yegor Popovych <erop1991@gmail.com>
2013-04-04 16:41:15 +0300

    Cleaned code in get_app_path

--- compressor/utils/apppath.py
+++ compressor/utils/apppath.py
@@ -2,22 +2,17 @@
 import os
 from django.conf import settings
 
-__app_paths = None
-
 def __get_app_paths():
-    if __app_paths is None:
-	app_paths = []
-        for app in settings.INSTALLED_APPS:
-            module = __import__(app)
-            app_paths.append(dir(module))
-	__app_paths = app_paths
-    return __app_paths
+    app_paths = set()
+    for app in settings.INSTALLED_APPS:
+        module = __import__(app)
+        app_paths.add(module.__path__[0])
+    return app_paths
 
-__get_app_paths()
+__app_paths = __get_app_paths()
 
 def get_app_path_for_filepath(file_path):
-    app_paths = __get_app_paths()
-    for path in app_paths:
+    for path in __app_paths:
         if file_path.startswith(path):
             return path
     return None
