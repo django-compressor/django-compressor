ed689a72bf694ee6e7415616cd3bce5a73c1d66d
Yegor Popovych <erop1991@gmail.com>
2013-05-04 17:55:44 +0300

    Fixed apppath for STATIC_ROOT handle

--- compressor/utils/apppath.py
+++ compressor/utils/apppath.py
@@ -12,31 +12,55 @@ def __get_app_paths():
 
     return app_paths
 
-def __get_root_paths():
-    root_paths = set()
-    root_paths.add(settings.STATIC_ROOT)
-    for path in settings.STATICFILES_DIRS:
-        root_paths.add(path)
-    return root_paths
-
 __app_paths = __get_app_paths()
-__root_paths = __get_root_paths()
+
+def search_in_apps(file):
+    try:
+        file_path = AppDirectoriesFinder().find(file)
+        if len(file_path) > 0:
+            if isinstance(file_path, list):
+                file_path = file_path[0]
+            return file_path
+        return None
+    except:
+        return None
+
+def search_in_dirs(file):
+    for dir in settings.STATICFILES_DIRS:
+        path = os.path.join(dir, file)
+        if os.path.isfile(path):
+            return os.path.dirname(dir)
+    return None
+
+def get_filename(dir, full_path):
+    file = full_path.replace(dir, '')
+    if file[:1] == '/':
+        file = file[1:]
+    return file
 
 def get_app_path_for_filepath(file_path):
-    for path in __root_paths:
-        if file_path.startswith(path):
-            file = file_path.replace(path, '')
-            if file[:1] == '/':
-                file = file[1:]
-            try:
-                file_path = AppDirectoriesFinder().find(file)
-                if len(file_path) == 0:
-                    return os.path.dirname(path)
-            except:
-                return None
-    for path in __app_paths:
-        if file_path.startswith(path):
-            return path
-    return None
+    if file_path.startswith(settings.STATIC_ROOT):
+        file = get_filename(settings.STATIC_ROOT, file_path)
+        file_path = search_in_apps(file)
+        if file_path is None:
+            path = search_in_dirs(file)
+            if path is not None:
+                return path
+    else:
+        for dir in settings.STATICFILES_DIRS:
+            if file_path.startswith(dir):
+                file = get_filename(dir, file_path)
+                path = search_in_apps(file)
+                if path is None:
+                    return search_in_dirs(file)
+                else:
+                    file_path = path
+                break
+
+    if file_path is not None:
+        for path in __app_paths:
+            if file_path.startswith(path):
+                return path
+    return settings.STATIC_ROOT
 
 
