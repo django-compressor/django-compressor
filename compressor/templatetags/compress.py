eedd3aec706e675e2ac8287326ed51597536dfb7
Jannis Leidel <jannis@leidel.info>
2013-05-27 16:52:26 +0200

    Removed raising the exception in template tag.

--- compressor/templatetags/compress.py
+++ compressor/templatetags/compress.py
@@ -111,7 +111,6 @@ class CompressorMixin(object):
             assert isinstance(rendered_output, six.text_type)
             return rendered_output
         except Exception:
-            raise
             if settings.DEBUG or forced:
                 raise
 
