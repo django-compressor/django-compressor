a3d7efaa6b9ef7074c81ddcd814ddb39834f5473
fsw <frank@wawrzak.com>
2013-08-30 09:26:17 +0200

    changed DEBUG_TOGGLE from 'None' to None

--- compressor/conf.py
+++ compressor/conf.py
@@ -12,7 +12,7 @@ class CompressorConf(AppConf):
     # Allows changing verbosity from the settings.
     VERBOSE = False
     # GET variable that disables compressor e.g. "nocompress"
-    DEBUG_TOGGLE = 'None'
+    DEBUG_TOGGLE = None
     # the backend to use when parsing the JavaScript or Stylesheet files
     PARSER = 'compressor.parser.AutoSelectParser'
     OUTPUT_DIR = 'CACHE'
