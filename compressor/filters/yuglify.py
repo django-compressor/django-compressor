326b36dff6549957b3fdf5f1a7a398babd1c2345
Chris Kief <chriskief@mbp.home>
2013-11-16 13:28:44 -0500

    Support for yUglify

---
+++ compressor/filters/yuglify.py
@@ -0,0 +1,26 @@
+from compressor.conf import settings
+from compressor.filters import CompilerFilter
+
+
+class YUglifyFilter(CompilerFilter):
+    command = "{binary} {args}"
+
+    def __init__(self, *args, **kwargs):
+        super(YUglifyFilter, self).__init__(*args, **kwargs)
+        self.command += ' --type=%s' % self.type
+
+
+class YUglifyCSSFilter(YUglifyFilter):
+    type = 'css'
+    options = (
+        ("binary", settings.COMPRESS_YUGLIFY_BINARY),
+        ("args", settings.COMPRESS_YUGLIFY_CSS_ARGUMENTS),
+    )
+
+
+class YUglifyJSFilter(YUglifyFilter):
+    type = 'js'
+    options = (
+        ("binary", settings.COMPRESS_YUGLIFY_BINARY),
+        ("args", settings.COMPRESS_YUGLIFY_JS_ARGUMENTS),
+    )
