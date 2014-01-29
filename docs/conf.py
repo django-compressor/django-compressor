a6a2b2b0b952c66807a5d96d64022628948ad60c
Jannis Leidel <jannis@leidel.info>
2014-01-29 10:17:34 +0100

    Force the new theme.

--- docs/conf.py
+++ docs/conf.py
@@ -97,6 +97,7 @@ pygments_style = 'murphy'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'default'
+RTD_NEW_THEME = True
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
