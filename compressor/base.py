503c3c8c9906c0990ed9961974844c0c05a8e049
Aurélien Bompard <aurelien@bompard.org>
2014-03-29 16:56:18 +0100

    In debug mode, use the app's static dir first
    In debug mode, files should always be compressed based on the app's
    static directory, not the global static directory, where files can be
    outdated.
    
    Fixes: #425

--- compressor/base.py
+++ compressor/base.py
@@ -117,18 +117,19 @@ class Compressor(object):
         get_filename('css/one.css') -> '/full/path/to/static/css/one.css'
         """
         filename = None
-        # first try finding the file in the root
-        try:
-            # call path first so remote storages don't make it to exists,
-            # which would cause network I/O
-            filename = self.storage.path(basename)
-            if not self.storage.exists(basename):
-                filename = None
-        except NotImplementedError:
-            # remote storages don't implement path, access the file locally
-            if compressor_file_storage.exists(basename):
-                filename = compressor_file_storage.path(basename)
-        # secondly try to find it with staticfiles (in debug mode)
+        if not settings.DEBUG:
+            # first try finding the file in the root
+            try:
+                # call path first so remote storages don't make it to exists,
+                # which would cause network I/O
+                filename = self.storage.path(basename)
+                if not self.storage.exists(basename):
+                    filename = None
+            except NotImplementedError:
+                # remote storages don't implement path, access the file locally
+                if compressor_file_storage.exists(basename):
+                    filename = compressor_file_storage.path(basename)
+        # secondly try to find it with staticfiles (and always in debug mode)
         if not filename and self.finders:
             filename = self.finders.find(url2pathname(basename))
         if filename:
--- compressor/tests/test_base.py
+++ compressor/tests/test_base.py
@@ -1,6 +1,8 @@
 from __future__ import with_statement, unicode_literals
 import os
 import re
+from tempfile import mkdtemp
+from shutil import rmtree, copytree
 
 try:
     from bs4 import BeautifulSoup
@@ -14,11 +16,12 @@ from django.test.utils import override_settings
 
 from compressor import cache as cachemod
 from compressor.base import SOURCE_FILE, SOURCE_HUNK
-from compressor.cache import get_cachekey, get_precompiler_cachekey
+from compressor.cache import get_cachekey, get_precompiler_cachekey, get_hexdigest
 from compressor.conf import settings
 from compressor.css import CssCompressor
 from compressor.exceptions import FilterDoesNotExist, FilterError
 from compressor.js import JsCompressor
+from compressor.storage import DefaultStorage
 
 
 def make_soup(markup):
@@ -337,3 +340,48 @@ class CacheTestCase(SimpleTestCase):
             get_precompiler_cachekey("asdf", "asdf")
         except TypeError:
             self.fail("get_precompiler_cachekey raised TypeError unexpectedly")
+
+
+class CompressorInDebugModeTestCase(SimpleTestCase):
+
+    def setUp(self):
+        settings.COMPRESS_ENABLED = True
+        settings.COMPRESS_PRECOMPILERS = ()
+        settings.COMPRESS_DEBUG_TOGGLE = 'nocompress'
+        settings.DEBUG = True
+        self.css = '<link rel="stylesheet" href="/static/css/one.css" type="text/css" />'
+        self.tmpdir = mkdtemp()
+        copytree(settings.STATIC_ROOT, os.path.join(self.tmpdir, "static"))
+        self.old_compress_root = settings.COMPRESS_ROOT
+        settings.STATIC_ROOT = settings.COMPRESS_ROOT = os.path.join(self.tmpdir, "static")
+        settings.STATICFILES_DIRS = [self.old_compress_root]
+
+    def tearDown(self):
+        rmtree(self.tmpdir)
+        settings.DEBUG = False
+        settings.STATIC_ROOT = settings.COMPRESS_ROOT = self.old_compress_root
+        delattr(settings, "STATICFILES_DIRS")
+
+    def test_filename_in_debug_mode(self):
+        # In debug mode, files should always be compressed based on the app's
+        # static directory, not the global static directory, where files can
+        # be outdated
+        css_filename = os.path.join(settings.COMPRESS_ROOT, "css", "one.css")
+        # Store the hash of the original file's content
+        css_content = open(css_filename).read()
+        hashed = get_hexdigest(css_content, 12)
+        # Now modify the file in the STATIC_ROOT
+        test_css_content = "p { font-family: 'test' }"
+        with open(css_filename, "a") as css:
+            css.write("\n")
+            css.write(test_css_content)
+        # We should generate a link with the hash of the original content, not
+        # the modified one
+        expected = '<link rel="stylesheet" href="/static/CACHE/css/%s.css" type="text/css" />' % hashed
+        compressor = CssCompressor(self.css)
+        compressor.storage = DefaultStorage()
+        output = compressor.output()
+        self.assertEqual(expected, output)
+        result = open(os.path.join(settings.COMPRESS_ROOT, "CACHE", "css",
+                                   "%s.css" % hashed), "r").read()
+        self.assertTrue(test_css_content not in result)
