87d5fd7e5e852ab227c1b3e73c52f6c87a00f4df
Nicolas Ferrari <nferrari@alwaysdata.com>
2015-03-13 18:31:30 +0100

    Specify default DJANGO_MODULE_SETTINGS.

--- compressor/conf.py
+++ compressor/conf.py
@@ -80,7 +80,7 @@ class CompressorConf(AppConf):
 
     class Meta:
         prefix = 'compress'
-        holder = os.environ['DJANGO_SETTINGS_MODULE']
+        holder = os.getenv('DJANGO_SETTINGS_MODULE', 'django.conf.settings')
 
     def configure_root(self, value):
         # Uses Django's STATIC_ROOT by default
