188ac7a0090d7453dd79c38784ae0088f476b28b
René Fleschenberg <rene@fleschenberg.net>
2015-06-30 19:30:08 +0200

    Fix issue #626

--- compressor/utils/staticfiles.py
+++ compressor/utils/staticfiles.py
@@ -1,10 +1,19 @@
 from __future__ import absolute_import, unicode_literals
 
+import django
 from django.core.exceptions import ImproperlyConfigured
 
 from compressor.conf import settings
 
-if "django.contrib.staticfiles" in settings.INSTALLED_APPS:
+
+def staticfiles_installed():
+    if django.VERSION < (1, 7):
+        return "django.contrib.staticfiles" in settings.INSTALLED_APPS
+    from django.apps import apps
+    return apps.is_installed("django.contrib.staticfiles")
+
+
+if staticfiles_installed():
     from django.contrib.staticfiles import finders  # noqa
 
     if ("compressor.finders.CompressorFinder"
