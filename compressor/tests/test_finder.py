56c376f2460fa5fde555875849bb60389bd91281
René Fleschenberg <rene@fleschenberg.net>
2015-06-30 23:47:54 +0200

    Add a very simple testcase for compressor.finders.

---
+++ compressor/tests/test_finder.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from compressor.finders import CompressorFinder
+from compressor.storage import CompressorFileStorage
+
+
+class FinderTestCase(TestCase):
+
+    def test_has_correct_storage(self):
+        finder = CompressorFinder()
+        self.assertTrue(type(finder.storage) is CompressorFileStorage)
+
+    def test_list_returns_empty_list(self):
+        finder = CompressorFinder()
+        self.assertEquals(finder.list([]), [])
