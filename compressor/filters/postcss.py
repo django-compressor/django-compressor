0a6a19ff4bbf5f89899c7e2842e830d47207d15f
Pithikos <manossef@hotmail.com>
2017-01-13 22:00:54 +0000

    Add tests for PostCSSFilter

--- compressor/filters/postcss.py
+++ compressor/filters/postcss.py
@@ -7,14 +7,22 @@ DEFAULT_ARGS = " "
 DEFAULT_PLUGINS = ()
 
 
-class PostCSSFilter(CompilerFilter):
-
+def fetch_options():
     def plugins_as_args(plugins):
         return ''.join(map(lambda plugin: '--use %s ' % plugin, plugins))
-
-    command = "{binary} {args} {plugins} -o {outfile} {infile}"
-    options = (
+    return (
         ("binary", getattr(settings, "COMPRESS_POSTCSS_BINARY", DEFAULT_BINARY)),
         ("args", getattr(settings, "COMPRESS_POSTCSS_ARGS", DEFAULT_ARGS)),
         ("plugins", plugins_as_args(getattr(settings, "COMPRESS_POSTCSS_PLUGINS", DEFAULT_PLUGINS))),
     )
+
+
+class PostCSSFilter(CompilerFilter):
+    command = "{binary} {args} {plugins} -o {outfile} {infile}"
+    options = fetch_options()
+
+
+class PostCSSFilterTestable(PostCSSFilter):
+    def __init__(self, *args, **kwargs):
+        super(PostCSSFilter, self).__init__(*args, **kwargs)
+        self.options = fetch_options()
