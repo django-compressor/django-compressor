5300605acc875ef0def87cfb859b3423f2fda644
Jacob Rief <jacob.rief@gmail.com>
2018-08-03 21:19:15 +0200

    add support to include external and deferred files (#891)
    * add support to include external and deferred files
    
    * add documentation for enhanced Sekizai compressor
    
    * add dummy testing file
    
    * add unit test to check Sekizai processor
    
    * changes for next version
    
    * rebased to current version
    
    * Update django-sekizai.txt

--- compressor/contrib/sekizai.py
+++ compressor/contrib/sekizai.py
@@ -6,6 +6,11 @@
  and: https://github.com/ojii/django-sekizai.git@0.6 or later
 """
 from compressor.templatetags.compress import CompressorNode
+from compressor.exceptions import UncompressableFileError
+from compressor.base import Compressor
+from compressor.conf import settings
+from compressor.utils import get_class
+
 from django.template.base import TextNode
 
 
@@ -15,4 +20,44 @@ def compress(context, data, name):
     Name is either 'js' or 'css' (the sekizai namespace)
     Basically passes the string through the {% compress 'js' %} template tag
     """
-    return CompressorNode(nodelist=TextNode(data), kind=name, mode='file').render(context=context)
+    # separate compressable from uncompressable files
+    parser = get_class(settings.COMPRESS_PARSER)(data)
+    js_compressor, css_compressor = Compressor('js'), Compressor('css')
+    compressable_elements, expanded_elements, deferred_elements = [], [], []
+    if name == 'js':
+        for elem in parser.js_elems():
+            attribs = parser.elem_attribs(elem)
+            try:
+                if 'src' in attribs:
+                    js_compressor.get_basename(attribs['src'])
+            except UncompressableFileError:
+                if 'defer' in attribs:
+                    deferred_elements.append(elem)
+                else:
+                    expanded_elements.append(elem)
+            else:
+                compressable_elements.append(elem)
+    elif name == 'css':
+        for elem in parser.css_elems():
+            attribs = parser.elem_attribs(elem)
+            try:
+                if parser.elem_name(elem) == 'link' and attribs['rel'].lower() == 'stylesheet':
+                    css_compressor.get_basename(attribs['href'])
+            except UncompressableFileError:
+                expanded_elements.append(elem)
+            else:
+                compressable_elements.append(elem)
+
+    # reconcatenate them
+    data = ''.join(parser.elem_str(e) for e in expanded_elements)
+    expanded_node = CompressorNode(nodelist=TextNode(data), kind=name, mode='file')
+    data = ''.join(parser.elem_str(e) for e in compressable_elements)
+    compressable_node = CompressorNode(nodelist=TextNode(data), kind=name, mode='file')
+    data = ''.join(parser.elem_str(e) for e in deferred_elements)
+    deferred_node = CompressorNode(nodelist=TextNode(data), kind=name, mode='file')
+
+    return '\n'.join([
+        expanded_node.get_original_content(context=context),
+        compressable_node.render(context=context),
+        deferred_node.get_original_content(context=context),
+    ])
---
+++ compressor/tests/test_sekizai.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import with_statement, unicode_literals
+
+from django.template import Template
+from django.test import TestCase
+from sekizai.context import SekizaiContext
+
+
+class TestSekizaiCompressorExtension(TestCase):
+    """
+    Test case for Sekizai extension.
+    """
+    def test_postprocess_js(self):
+        template_string = '''
+{% load static compress sekizai_tags %}
+{% addtoblock "js" %}<script src="{% static 'js/one.js' %}" type="text/javascript"></script>{% endaddtoblock %}
+{% addtoblock "js" %}<script async="async" defer="defer" src="https://maps.googleapis.com/maps/api/js?key={{ apiKey }}"></script>{% endaddtoblock %}
+{% addtoblock "js" %}<script src="{% static 'js/two.js' %}" type="text/javascript"></script>{% endaddtoblock %}
+{% addtoblock "js" %}<script src="https://code.jquery.com/jquery-3.3.1.min.js" type="text/javascript"></script>{% endaddtoblock %}
+{% addtoblock "js" %}<script src="{% static 'js/three.js' %}" type="text/javascript"></script>{% endaddtoblock %}
+{% render_block "js" postprocessor "compressor.contrib.sekizai.compress" %}'''
+        template = Template(template_string)
+        context = SekizaiContext({'apiKey': 'XYZ'})
+        html = template.render(context).strip()
+        self.assertEqual(html,
+'''<script src="https://code.jquery.com/jquery-3.3.1.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="/static/CACHE/js/output.cb5ca6a608a4.js"></script>
+<script async="async" defer="defer" src="https://maps.googleapis.com/maps/api/js?key=XYZ"></script>''')
+
+    def test_postprocess_css(self):
+        template_string = '''
+{% load static compress sekizai_tags %}
+{% addtoblock "css" %}<link href="{% static 'css/one.css' %}" rel="stylesheet" type="text/css" />{% endaddtoblock %}
+{% addtoblock "css" %}<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" rel="stylesheet" type="text/css" />{% endaddtoblock %}
+{% addtoblock "css" %}<link href="{% static 'css/two.css' %}" rel="stylesheet" type="text/css" />{% endaddtoblock %}
+{% render_block "css" postprocessor "compressor.contrib.sekizai.compress" %}'''
+        template = Template(template_string)
+        context = SekizaiContext()
+        html = template.render(context).strip()
+        self.assertEqual(html,
+'''<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="/static/CACHE/css/output.20f9b535162f.css" type="text/css" />''')
