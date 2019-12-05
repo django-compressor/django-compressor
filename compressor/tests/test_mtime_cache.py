98c637b2667427656fa67c6ff9c1f2bc3bf020f7
Albert Wang <git@albertyw.com>
2019-12-05 11:26:19 -0800

    Remove django.utils.six (#969)

--- compressor/tests/test_mtime_cache.py
+++ compressor/tests/test_mtime_cache.py
@@ -1,7 +1,7 @@
 from django.core.management import call_command
 from django.core.management.base import CommandError
 from django.test import TestCase
-from django.utils.six import StringIO
+from six import StringIO
 
 
 class TestMtimeCacheCommand(TestCase):
