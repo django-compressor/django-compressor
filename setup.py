3445db701fbb396d6620a574f8371a3ed8b39080
Albert Wang <git@albertyw.com>
2019-12-20 00:31:37 -0800

    Update django-appconf

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ setup(
     ],
     zip_safe=False,
     install_requires=[
-        'django-appconf >= 1.0',
+        'django-appconf >= 1.0.3',
         'rcssmin == 1.0.6',
         'rjsmin == 1.1.0',
         'six >= 1.12.0',
