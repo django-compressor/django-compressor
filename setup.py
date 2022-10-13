693616ab5d2c4dc2dfff396a2863efb2cc96dd09
Mathieu Pillard <diox@users.noreply.github.com>
2022-10-14 00:36:29 +0200

    Drop Python 3.6 support (#1148)

--- setup.py
+++ setup.py
@@ -139,7 +139,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
