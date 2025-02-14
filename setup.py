21a114b51f8b8ead33b7cc4f2af84a5ec38cf2f4
Carlton Gibson <carlton.gibson@noumenal.es>
2025-02-14 16:48:43 +0100

    Officially support Django 5.2

--- setup.py
+++ setup.py
@@ -149,6 +149,7 @@ setup(
         "Framework :: Django :: 4.2",
         "Framework :: Django :: 5.0",
         "Framework :: Django :: 5.1",
+        "Framework :: Django :: 5.2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
